cd6dbec827bf243f1cae2bdbbaf09feeb5988121
jongwoo315 <jongwoo315@gmail.com>
2026-02-13 01:10:15 +0900

    feat: ProcessDataUseCase 애플리케이션 유스케이스 추가

---
+++ tests/data_pipeline/test_use_cases.py
@@ -0,0 +1,19 @@
+import inspect
+
+from services.data_pipeline.application.use_cases import ProcessDataUseCase
+
+
+class TestProcessDataUseCaseInterface:
+    def test_is_abstract(self):
+        try:
+            ProcessDataUseCase()
+            assert False, "Should not instantiate abstract class"
+        except TypeError:
+            pass
+
+    def test_has_execute_method(self):
+        assert hasattr(ProcessDataUseCase, "execute")
+        sig = inspect.signature(ProcessDataUseCase.execute)
+        params = list(sig.parameters.keys())
+        assert "source" in params
+        assert "destination" in params
